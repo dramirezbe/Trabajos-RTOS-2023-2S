03dc050f0f306e4d047465ed17d6be0839197479
dramirezbe <dramirezbe@unal.edu.co>
2023-09-05 21:45:06 -0500

    Entrega Tarea

--- main/main.c
+++ main/main.c
@@ -7,16 +7,26 @@
 
 #define led 33
 #define button 32
-#define STACK_SIZE 1024
+#define STACK_SIZE 1024*2
+uint8_t count=0;
 
 esp_err_t init_led(void);
 esp_err_t create_tasks(void);
 esp_err_t init_button(void);
+void isr_handler(void *args);
 
 void vTaskLed(void *pvParameters);
 void vTaskButtonInterruptor(void *pvParameters);
 void app_main(void)
 {
+    init_led();
+    init_button();
+    create_tasks();
+    if (count==1)
+    {
+        gpio_set_level(led,0);
+        vTaskDelay(500);
+    }
     
 }
 esp_err_t init_led(void)
@@ -27,10 +37,19 @@ esp_err_t init_led(void)
 }
 esp_err_t init_button(void)
 {
-    gpio_reset_pin(button);
-    gpio_set_direction(button, GPIO_MODE_INPUT);
+    gpio_config_t pGPIOConfig;
+    pGPIOConfig.pin_bit_mask = (1ULL << button); 
+    pGPIOConfig.mode =GPIO_MODE_DEF_INPUT;
+    pGPIOConfig.pull_up_en =GPIO_PULLUP_DISABLE;
+    pGPIOConfig.pull_down_en =GPIO_PULLDOWN_DISABLE;
+    pGPIOConfig.intr_type =GPIO_INTR_NEGEDGE;
+
+    gpio_config(&pGPIOConfig);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(button, isr_handler, NULL);
     return ESP_OK;
 }
+
 esp_err_t create_tasks(void)
 {
     static uint8_t ucParameterToPass;
@@ -61,4 +80,12 @@ void vTaskLed(void *pvParameters)
         gpio_set_level(led,0);
         vTaskDelay(250);
     }
+}
+void vTaskButtonInteruptor(void *pvParameters)
+{
+    
+}
+void isr_handler(void *args)
+{
+    count ++;
 }
